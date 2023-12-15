1118da0c25bc6ac16dba0f51fe63b8f15eb45857
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-12-15 23:10:56 +0530

    datatype-summary completed

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -4,15 +4,15 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 500
-const scoreValue = 500.3
+const score = 500;
+const scoreValue = 500.3;
 
-const isLoggedIn = false
-const outsideTemp = null
+const isLoggedIn = false;
+const outsideTemp = null;
 let userEmail;
 
-const id = Symbol('423')
-const anotherId = Symbol('423')
+const id = Symbol("423");
+const anotherId = Symbol("423");
 
 // console.log(id === anotherId);
 
@@ -22,13 +22,13 @@ const anotherId = Symbol('423')
 
 const heros = ["pokemon", "hatim", "antman"];
 let myObj = {
-    name: "remo",
-    age: 22,
-}
+  name: "remo",
+  age: 22,
+};
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+const myFunction = function () {
+  console.log("Hello world");
+};
 
 console.log(typeof anotherId);
 
@@ -49,4 +49,65 @@ console.log(typeof anotherId);
        Arrays  =>  object
        Function  =>  function | Object Function
        Object  =>  object
+ */
+
+// Symbol() === Symbol(); //false
+// console.log("Is both symbols are equal? ", Symbol() === Symbol());
+
+// Symbol("JavaScript") === Symbol("Programming");
+// console.log(
+//   "Is both symbols with description are equal? ",
+//   Symbol("JavaScript") === Symbol("Programming")
+// );
+
+// let mySymbol1 = Symbol("symDescription");
+// let mySymbol2 = Symbol("symDescription");
+// console.log("Two symbols with the same description equal? :",mySymbol1 === mySymbol2);
+
+// console.log(Symbol()); //Symbol()
+// console.log(Symbol("JavaScript")); //Symbol(JavaScript)
+
+// console.log("type of symbol:", typeof Symbol()); //symbol
+// console.log("type of symbol with description:", typeof mySymbol1); //symbol
+// console.log("Symbols are of typeof symbol:", typeof Symbol() === "symbol"); //true
+// console.log("A symbol is never equal to anything else except itself.",mySymbol1 === mySymbol1);
+
+// let mySym = new Symbol(); //TypeError throw when using new operator
+
+// Stack (Primitive) The datatypes that is Primitive uses Stack Memory
+// Heap (Non-Primitive) The datatypes that is Non-Primitive uses Heap Memory
+
+let username = "demouser";
+
+let newusername = username;
+newusername = "nayahai";
+
+/**
+ * When we pass a value to variable that is primtive daatatype it gives a
+ * copy of it  
+ */
+console.log(username);
+console.log(newusername);
+
+
+
+let userdetails = {
+  email: userEmail,
+  dob : "12/07/78",
+}
+
+let newuserdetails = userdetails
+
+/**
+ * When we pass a value to variable that is non-primtive daatatype it gives a
+ * reference of it & not the copy of it.
+ */
+
+newuserdetails.email = "newuser@co.in"
+
+console.log(userdetails.email);
+console.log(userdetails.email);
+
+/***
+ * For better visual explanation have a look at img (stack-heap.png)
  */
