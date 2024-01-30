f077f821bd8fa2e96c4cb8470db4e4685b47a5ab
Pujari Basvaraj <basvarajpujari607@gmail.com>
2024-01-30 20:25:44 +0530

    till 100 qstns

--- React-Prep/react-basics/src/components/HomePage.jsx
+++ React-Prep/react-basics/src/components/HomePage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
+/**
+ * 1.Using useState hook with Object Spread Syntax(user object)
+ * 2. Using Callback Function in useState(student object)
+ */
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const [user, setUser] = useState({ name: "", age: 20 });
+  const [student, setstudent] = useState(() => {
+    return { sname: "", age: 20 };
+  });
 
-export default HomePage
+  // console.log(user);
+
+  const updateName = (newName) => {
+    setUser({ ...user, name: newName });
+  };
+
+  const updateStud = (newName) => {
+    setstudent((prevStudent) => {
+      return { ...prevStudent, sname: newName };
+    });
+  };
+
+  // console.log(user);
+  console.log(student);
+  return (
+    <div>
+      {/* <input value={user.name} onChange={(e) => updateName(e.target.value)} /> */}
+
+      <input value={student.sname} onChange={(e) => updateStud(e.target.value)} />
+    </div>
+  );
+};
+
+export default HomePage;
